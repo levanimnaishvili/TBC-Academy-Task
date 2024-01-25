d839bfb9cf4299f3d01c5ac6e6d5c27f0870f863
Levani <levani.imna@gmail.com>
2024-01-25 12:03:51 -0800

    fix: temporary fix

--- carousel.js
+++ carousel.js
@@ -9,100 +9,106 @@ let navBullets = [];
 let reminder = slideCount % 3;
 
 window.addEventListener("load", () => {
-  for (let i = 0; i < slideCount; i++) {
-    let slide = document.createElement("li");
-    slide.textContent = i;
-    let width = 200;
-    let height = 60;
+  carousel && carousel.addEventListener("load", () => {
+    for (let i = 0; i < slideCount; i++) {
+      let slide = document.createElement("li");
+      slide.textContent = i;
+      let width = 200;
+      let height = 60;
 
-    //dimensions are required to compute proper position of elements
-    let posTop = `${carousel.clientHeight / 2 - height / 2}px`;
-    let leftX = `${carousel.clientWidth / 2 - width / 2}px`;
+      //dimensions are required to compute proper position of elements
+      let posTop = `${carousel.clientHeight / 2 - height / 2}px`;
+      let leftX = `${carousel.clientWidth / 2 - width / 2}px`;
 
-    //psitions if slide count has reminder, if reminder is 1 position will be mid, we need to calculate position for rem-2 case
-    let twoSlidePosX = `${carousel.clientWidth / 3 - width / 2}px`;
+      //psitions if slide count has reminder, if reminder is 1 position will be mid, we need to calculate position for rem-2 case
+      let twoSlidePosX = `${carousel.clientWidth / 3 - width / 2}px`;
 
-    slide.style.position = "absolute";
-    slide.style.width = `${width}px`;
-    slide.style.height = `${height}px`;
-    slide.style.top = posTop;
+      slide.style.position = "absolute";
+      slide.style.width = `${width}px`;
+      slide.style.height = `${height}px`;
+      slide.style.top = posTop;
 
-    //initially all element except first three are hidden
-    slide.style.opacity = 0;
+      //initially all element except first three are hidden
+      slide.style.opacity = 0;
 
-    if (i == 0 || i == 1 || i == 2) {
-      slide.style.opacity = 1;
-    }
+      if (i == 0 || i == 1 || i == 2) {
+        slide.style.opacity = 1;
+      }
 
-    slide.style.backgroundColor = `rgb(${Math.random() * 255}, ${
-      Math.random() * 255
-    }, ${Math.random() * 255})`;
+      slide.style.backgroundColor = `rgb(${Math.random() * 255}, ${
+        Math.random() * 255
+      }, ${Math.random() * 255})`;
 
-    switch (i % 3) {
-      case 0:
-        // reminder == 1
-        // არის მასივის პირველი წევრი
-        // შესაძლოა იყოს ერთადერთი ელემენტი ან პირველი ორიდან
-        if (i + 2 < slideCount) slide.style.left = 0;
-        else if (i + 2 == slideCount) slide.style.left = twoSlidePosX;
-        else {
-          slide.style.left = leftX;
-          let bullet = createBullet(i, 1);
-          navBullets.push(bullet);
-        }
-        break;
-      case 1:
-        // reminder == 2
-        // არის მასივის მეორე წევრი
-        // შესაძლოა იყოს დარჩენილებიდან მეორე
-        if (i + 1 < slideCount) {
-          slide.style.left = leftX;
-          if (slideCount > 3) {
-            let bullet = createBullet(i);
+      switch (i % 3) {
+        case 0:
+          // reminder == 1
+          // არის მასივის პირველი წევრი
+          // შესაძლოა იყოს ერთადერთი ელემენტი ან პირველი ორიდან
+          if (i + 2 < slideCount) slide.style.left = 0;
+          else if (i + 2 == slideCount) slide.style.left = twoSlidePosX;
+          else {
+            slide.style.left = leftX;
+            let bullet = createBullet(i, 1);
             navBullets.push(bullet);
           }
-        } else {
-          slide.style.right = twoSlidePosX;
-          let bullet = createBullet(i, 2);
-          navBullets.push(bullet);
-        }
-        break;
-      case 2:
-        // reminder == 0
-        // არის მასივის მესამე წევრი
-        // ვერ იქნება ნარჩენებში
-        slide.style.right = 0;
-        break;
+          break;
+        case 1:
+          // reminder == 2
+          // არის მასივის მეორე წევრი
+          // შესაძლოა იყოს დარჩენილებიდან მეორე
+          if (i + 1 < slideCount) {
+            slide.style.left = leftX;
+            if (slideCount > 3) {
+              let bullet = createBullet(i);
+              navBullets.push(bullet);
+            }
+          } else {
+            slide.style.right = twoSlidePosX;
+            let bullet = createBullet(i, 2);
+            navBullets.push(bullet);
+          }
+          break;
+        case 2:
+          // reminder == 0
+          // არის მასივის მესამე წევრი
+          // ვერ იქნება ნარჩენებში
+          slide.style.right = 0;
+          break;
+      }
+      slides.push(slide);
     }
-    slides.push(slide);
-  }
 
-  for (let i = 0; i < slideCount; i++) {
-    carousel.appendChild(slides[i]);
-  }
-  if (navBullets) {
-    let bulletContainer = document.createElement("div");
-    bulletContainer.classList.add("bullets-container");
-    for (let bullet of navBullets) {
-      bulletContainer.appendChild(bullet);
+    for (let i = 0; i < slideCount; i++) {
+      carousel.appendChild(slides[i]);
     }
-    carousel.appendChild(bulletContainer);
-  }
-  let intervalId;
-  if (slideCount > 3) {
-    intervalId = setInterval(() => {
-      moveRight();
-    }, 3000);
-  }
-  carousel.addEventListener("mouseenter", () => {
-    if (intervalId) clearInterval(intervalId);
-  });
-  carousel.addEventListener("mouseleave", () => {
+    if (navBullets) {
+      let bulletContainer = document.createElement("div");
+      bulletContainer.classList.add("bullets-container");
+      for (let bullet of navBullets) {
+        bulletContainer.appendChild(bullet);
+      }
+      carousel.appendChild(bulletContainer);
+    }
+
+    let intervalId;
     if (slideCount > 3) {
       intervalId = setInterval(() => {
         moveRight();
       }, 3000);
     }
+    carousel.addEventListener("mouseenter", () => {
+      if (intervalId) clearInterval(intervalId);
+    });
+    carousel.addEventListener("mouseleave", () => {
+      if (slideCount > 3) {
+        intervalId = setInterval(() => {
+          moveRight();
+        }, 3000);
+      }
+    });
+
+    btnLeft.addEventListener("click", () => moveLeft());
+    btnRight.addEventListener("click", () => moveRight());
   });
 });
 
@@ -147,9 +153,6 @@ function moveRight() {
     showSlides(current);
   }
 }
-
-btnLeft.addEventListener("click", () => moveLeft());
-btnRight.addEventListener("click", () => moveRight());
 
 function createBullet(slideIndex, slidesToShow) {
   let bullet = document.createElement("button");
